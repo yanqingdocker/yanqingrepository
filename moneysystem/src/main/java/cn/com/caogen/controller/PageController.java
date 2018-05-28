bc1f4e3eb7592f9df7d432e1eae9de9536be54ad
zhanglijing <941257921@qq.com>
2018-05-28 09:38:05 +0800

    a

--- moneysystem/src/main/java/cn/com/caogen/controller/PageController.java
+++ moneysystem/src/main/java/cn/com/caogen/controller/PageController.java
@@ -12,14 +12,15 @@ import javax.servlet.http.HttpServletRequest;
  */
 @Controller
 public class PageController {
+    //    中文版
     /**
      * 访问index界面
      *
      * @returnmy/my_account
      */
-    @RequestMapping("/index")
-    public String index() {
-        return "index";
+    @RequestMapping("/CN/index")
+    public String CNindex() {
+        return "CN/index";
     }
 
     /**
@@ -27,9 +28,9 @@ public class PageController {
      *
      * @return
      */
-    @RequestMapping("/register")
-    public String register() {
-        return "register";
+    @RequestMapping("/CN/register")
+    public String CNregister() {
+        return "CN/register";
     }
 
     /**
@@ -37,12 +38,12 @@ public class PageController {
      *
      * @return
      */
-    @RequestMapping("/login")
-    public String login(HttpServletRequest httpServletRequest) {
+    @RequestMapping("/CN/login")
+    public String CNlogin(HttpServletRequest httpServletRequest) {
         if(JedisUtil.getUser(httpServletRequest) !=null){
-            return "index";
+            return "CN/index";
         }
-        return "login";
+        return "CN/login";
     }
 
     /**
@@ -50,9 +51,9 @@ public class PageController {
      *
      * @return
      */
-    @RequestMapping("/find_psw")
-    public String find_psw() {
-        return "find_psw";
+    @RequestMapping("/CN/find_psw")
+    public String CNfind_psw() {
+        return "CN/find_psw";
     }
 
     /**
@@ -60,27 +61,27 @@ public class PageController {
      *
      * @return
      */
-    @RequestMapping("/main/recharge_form")
-    public String recharge_form() {
-        return "main/recharge_form";
+    @RequestMapping("/CN/main/recharge_form")
+    public String CNrecharge_form() {
+        return "CN/main/recharge_form";
     }
     /**
      * 充值
      *
      * @return
      */
-    @RequestMapping("/main/recharge")
-    public String mainrecharge() {
-        return "main/recharge";
+    @RequestMapping("/CN/main/recharge")
+    public String CNmainrecharge() {
+        return "CN/main/recharge";
     }
     /**
      * 我的资料
      *
      * @return
      */
-    @RequestMapping("/my/my_account")
-    public String myaccount() {
-        return "my/my_account";
+    @RequestMapping("/CN/my/my_account")
+    public String CNmyaccount() {
+        return "CN/my/my_account";
     }
 
     /**
@@ -88,117 +89,117 @@ public class PageController {
      *
      * @return
      */
-    @RequestMapping("/my/authentication")
-    public String authentication() {
-        return "my/authentication";
+    @RequestMapping("/CN/my/authentication")
+    public String CNauthentication() {
+        return "CN/my/authentication";
     }
     /**
      * 我的余额
      *
      * @return
      */
-    @RequestMapping("/my/my_money")
-    public String my_money() {
-        return "my/my_money";
+    @RequestMapping("/CN/my/my_money")
+    public String CNmy_money() {
+        return "CN/my/my_money";
     }
     /**
      * 登录密码
      *
      * @return
      */
-    @RequestMapping("/my/login_psw")
-    public String login_psw() {
-        return "my/login_psw";
+    @RequestMapping("/CN/my/login_psw")
+    public String CNlogin_psw() {
+        return "CN/my/login_psw";
     }
     /**
      * 资金密码
      *
      * @return
      */
-    @RequestMapping("/my/pay_psw")
-    public String pay_psw() {
-        return "my/pay_psw";
+    @RequestMapping("/CN/my/pay_psw")
+    public String CNpay_psw() {
+        return "CN/my/pay_psw";
     }
     /**
      * 银行卡绑定
      *
      * @return
      */
-    @RequestMapping("/my/bank_card")
-    public String bank_card() {
-        return "my/bank_card";
+    @RequestMapping("/CN/my/bank_card")
+    public String CNbank_card() {
+        return "CN/my/bank_card";
     }
     /**
      * 支付宝微信/绑定
      *
      * @return
      */
-    @RequestMapping("/my/bind_account")
-    public String bind_account() {
-        return "my/bind_account";
+    @RequestMapping("/CN/my/bind_account")
+    public String CNbind_account() {
+        return "CN/my/bind_account";
     }
     /**
      * 更多--生活缴费
      *
      * @return
      */
-    @RequestMapping("/more/life_payment")
-    public String life_payment() {
-        return "more/life_payment";
+    @RequestMapping("/CN/more/life_payment")
+    public String CNlife_payment() {
+        return "CN/more/life_payment";
     }
     /**
      * 生活缴费--话费充值
      *
      * @return
      */
-    @RequestMapping("/more/phone_charge")
-    public String phone_charge() {
-        return "more/phone_charge";
+    @RequestMapping("/CN/more/phone_charge")
+    public String CNphone_charge() {
+        return "CN/more/phone_charge";
     }
     /**
      * 生活缴费--水费代缴
      *
      * @return
      */
-    @RequestMapping("/more/water_charge")
-    public String water_charge() {
-        return "more/water_charge";
+    @RequestMapping("/CN/more/water_charge")
+    public String CNwater_charge() {
+        return "CN/more/water_charge";
     }
     /**
      * 生活缴费--电费代缴
      *
      * @return
      */
-    @RequestMapping("/more/ele_charge")
-    public String ele_charge() {
-        return "more/ele_charge";
+    @RequestMapping("/CN/more/ele_charge")
+    public String CNele_charge() {
+        return "CN/more/ele_charge";
     }
     /**
      * 我的消息--写信
      *
      * @return
      */
-    @RequestMapping("/my/message")
-    public String message() {
-        return "my/message";
+    @RequestMapping("/CN/my/message")
+    public String CNmessage() {
+        return "CN/my/message";
     }
     /**
      * 我的消息--收件箱
      *
      * @return
      */
-    @RequestMapping("/my/inbox")
-    public String inbox() {
-        return "my/inbox";
+    @RequestMapping("/CN/my/inbox")
+    public String CNinbox() {
+        return "CN/my/inbox";
     }
     /**
      * 我的消息--发件箱
      *
      * @return
      */
-    @RequestMapping("/my/outbox")
-    public String outbox() {
-        return "my/outbox";
+    @RequestMapping("/CN/my/outbox")
+    public String CNoutbox() {
+        return "CN/my/outbox";
     }
 
     /**
@@ -206,112 +207,414 @@ public class PageController {
      *
      * @return
      */
-    @RequestMapping("/currency_exchange/exchange")
-    public String exchange() {
-        return "currency_exchange/exchange";
+    @RequestMapping("/CN/currency_exchange/exchange")
+    public String CNexchange() {
+        return "CN/currency_exchange/exchange";
     }
     /**
      * 交易明细列表
      * @return
      */
-    @RequestMapping("/currency_exchange/exchange_list")
-    public String exchange_list() {
-        return "currency_exchange/exchange_list"; }
+    @RequestMapping("/CN/currency_exchange/exchange_list")
+    public String CNexchange_list() {
+        return "CN/currency_exchange/exchange_list"; }
     /**
      * 汇率列表
      * @return
      */
-    @RequestMapping("/currency_exchange/exchange_rate_list")
-    public String exchange_rate_list() {
-        return "currency_exchange/exchange_rate_list"; }
+    @RequestMapping("/CN/currency_exchange/exchange_rate_list")
+    public String CNexchange_rate_list() {
+        return "CN/currency_exchange/exchange_rate_list"; }
     /**
      * 转账
      * @return
      */
-    @RequestMapping("/main/transfer_accounts")
-    public String transfer_accounts() {
-        return "main/transfer_accounts"; }
+    @RequestMapping("/CN/main/transfer_accounts")
+    public String CNtransfer_accounts() {
+        return "CN/main/transfer_accounts"; }
     /**
      * 提现
      * @return
      */
-    @RequestMapping("/main/trans_cash")
-    public String trans_cash() {
-        return "main/trans_cash"; }
+    @RequestMapping("/CN/main/trans_cash")
+    public String CNtrans_cash() {
+        return "CN/main/trans_cash"; }
     /**
      * 提现---从银行提现
      * @return
      */
-    @RequestMapping("/main/pos_Bank")
-    public String pos_Bank() {
-        return "main/pos_Bank"; }
+    @RequestMapping("/CN/main/pos_Bank")
+    public String CNpos_Bank() {
+        return "CN/main/pos_Bank"; }
     /**
      * 提现---从微信提现
      * @return
      */
-    @RequestMapping("/main/pos_WeChat")
-    public String pos_WeChat() {
-        return "main/pos_WeChat"; }
+    @RequestMapping("/CN/main/pos_WeChat")
+    public String CNpos_WeChat() {
+        return "CN/main/pos_WeChat"; }
 
     /**
      * 提现---从支付宝提现
      * @return
      */
-    @RequestMapping("/main/pos_Alipay")
-    public String pos_Alipay() {
-        return "main/pos_Alipay"; }
+    @RequestMapping("/CN/main/pos_Alipay")
+    public String CNpos_Alipay() {
+        return "CN/main/pos_Alipay"; }
     /**
      * 提现---现金提现
      * @return
      */
-    @RequestMapping("/main/pos_cash")
-    public String pos_cash() {
-        return "main/pos_cash"; }
+    @RequestMapping("/CN/main/pos_cash")
+    public String CNpos_cash() {
+        return "CN/main/pos_cash"; }
     /**
      * 提现---比特币提现
      * @return
      */
-    @RequestMapping("/main/pos_bitcoin")
-    public String pos_bitcoin() {
-        return "main/pos_bitcoin"; }
+    @RequestMapping("/CN/main/pos_bitcoin")
+    public String CNpos_bitcoin() {
+        return "CN/main/pos_bitcoin"; }
 
     /**
      * 无卡取现
      * @return
      */
-    @RequestMapping("/nonCardCash/cash_in")
-    public String nonCardCash() {
-        return "nonCardCash/cash_in"; }
+    @RequestMapping("/CN/nonCardCash/cash_in")
+    public String CNnonCardCash() {
+        return "CN/nonCardCash/cash_in"; }
 
     /**
      * 智慧理财
      * @return
      */
-    @RequestMapping("/more/financial")
-    public String financial() {
-        return "more/financial"; }
+    @RequestMapping("/CN/more/financial")
+    public String CNfinancial() {
+        return "CN/more/financial"; }
 
     /**
      * 交易记录
      * @return
      */
-    @RequestMapping("/more/trade_list")
-    public String trade_list() {
-        return "more/trade_list"; }
+    @RequestMapping("/CN/more/trade_list")
+    public String CNtrade_list() {
+        return "CN/more/trade_list"; }
     /**
      * 历史交易记录
      * @return
      */
-    @RequestMapping("/more/trade_history")
-    public String trade_history() {
-        return "more/trade_history"; }
+    @RequestMapping("/CN/more/trade_history")
+    public String CNtrade_history() {
+        return "CN/more/trade_history"; }
 
     /**
      * 活动记录
      * @return
      */
-    @RequestMapping("/more/active_list")
-    public String active_list() {
-        return "more/active_list"; }
+    @RequestMapping("/CN/more/active_list")
+    public String CNactive_list() {
+        return "CN/more/active_list"; }
+    //    以下为英文版
+    /**
+     * 访问index界面
+     *
+     * @returnmy/my_account
+     */
+    @RequestMapping("/EN/index")
+    public String ENindex() {
+        return "EN/index";
+    }
+
+    /**
+     * 访问注册页面
+     *
+     * @return
+     */
+    @RequestMapping("/EN/register")
+    public String ENregister() {
+        return "EN/register";
+    }
+
+    /**
+     * 访问登录页面
+     *
+     * @return
+     */
+    @RequestMapping("/EN/login")
+    public String ENlogin(HttpServletRequest httpServletRequest) {
+        if(JedisUtil.getUser(httpServletRequest) !=null){
+            return "EN/index";
+        }
+        return "EN/login";
+    }
+
+    /**
+     * 访问忘记密码界面
+     *
+     * @return
+     */
+    @RequestMapping("/EN/find_psw")
+    public String ENfind_psw() {
+        return "EN/find_psw";
+    }
+
+    /**
+     * 账户充值
+     *
+     * @return
+     */
+    @RequestMapping("/EN/main/recharge_form")
+    public String ENrecharge_form() {
+        return "EN/main/recharge_form";
+    }
+    /**
+     * 充值
+     *
+     * @return
+     */
+    @RequestMapping("/EN/main/recharge")
+    public String ENmainrecharge() {
+        return "EN/main/recharge";
+    }
+    /**
+     * 我的资料
+     *
+     * @return
+     */
+    @RequestMapping("/EN/my/my_account")
+    public String ENmyaccount() {
+        return "EN/my/my_account";
+    }
+
+    /**
+     * 账户认证/基本资料
+     *
+     * @return
+     */
+    @RequestMapping("/EN/my/authentication")
+    public String ENauthentication() {
+        return "EN/my/authentication";
+    }
+    /**
+     * 我的余额
+     *
+     * @return
+     */
+    @RequestMapping("/EN/my/my_money")
+    public String ENmy_money() {
+        return "EN/my/my_money";
+    }
+    /**
+     * 登录密码
+     *
+     * @return
+     */
+    @RequestMapping("/EN/my/login_psw")
+    public String ENlogin_psw() {
+        return "EN/my/login_psw";
+    }
+    /**
+     * 资金密码
+     *
+     * @return
+     */
+    @RequestMapping("/EN/my/pay_psw")
+    public String ENpay_psw() {
+        return "EN/my/pay_psw";
+    }
+    /**
+     * 银行卡绑定
+     *
+     * @return
+     */
+    @RequestMapping("/EN/my/bank_card")
+    public String ENbank_card() {
+        return "EN/my/bank_card";
+    }
+    /**
+     * 支付宝微信/绑定
+     *
+     * @return
+     */
+    @RequestMapping("/EN/my/bind_account")
+    public String ENbind_account() {
+        return "EN/my/bind_account";
+    }
+    /**
+     * 更多--生活缴费
+     *
+     * @return
+     */
+    @RequestMapping("/EN/more/life_payment")
+    public String ENlife_payment() {
+        return "EN/more/life_payment";
+    }
+    /**
+     * 生活缴费--话费充值
+     *
+     * @return
+     */
+    @RequestMapping("/EN/more/phone_charge")
+    public String ENphone_charge() {
+        return "EN/more/phone_charge";
+    }
+    /**
+     * 生活缴费--水费代缴
+     *
+     * @return
+     */
+    @RequestMapping("/EN/more/water_charge")
+    public String ENwater_charge() {
+        return "EN/more/water_charge";
+    }
+    /**
+     * 生活缴费--电费代缴
+     *
+     * @return
+     */
+    @RequestMapping("/EN/more/ele_charge")
+    public String ENele_charge() {
+        return "EN/more/ele_charge";
+    }
+    /**
+     * 我的消息--写信
+     *
+     * @return
+     */
+    @RequestMapping("/EN/my/message")
+    public String ENmessage() {
+        return "EN/my/message";
+    }
+    /**
+     * 我的消息--收件箱
+     *
+     * @return
+     */
+    @RequestMapping("/EN/my/inbox")
+    public String ENinbox() {
+        return "EN/my/inbox";
+    }
+    /**
+     * 我的消息--发件箱
+     *
+     * @return
+     */
+    @RequestMapping("/EN/my/outbox")
+    public String ENoutbox() {
+        return "EN/my/outbox";
+    }
+
+    /**
+     * 货币兑换
+     *
+     * @return
+     */
+    @RequestMapping("/EN/currency_exchange/exchange")
+    public String ENexchange() {
+        return "EN/currency_exchange/exchange";
+    }
+    /**
+     * 交易明细列表
+     * @return
+     */
+    @RequestMapping("/EN/currency_exchange/exchange_list")
+    public String ENexchange_list() {
+        return "EN/currency_exchange/exchange_list"; }
+    /**
+     * 汇率列表
+     * @return
+     */
+    @RequestMapping("/EN/currency_exchange/exchange_rate_list")
+    public String ENexchange_rate_list() {
+        return "EN/currency_exchange/exchange_rate_list"; }
+    /**
+     * 转账
+     * @return
+     */
+    @RequestMapping("/EN/main/transfer_accounts")
+    public String ENtransfer_accounts() {
+        return "EN/main/transfer_accounts"; }
+    /**
+     * 提现
+     * @return
+     */
+    @RequestMapping("/EN/main/trans_cash")
+    public String ENtrans_cash() {
+        return "EN/main/trans_cash"; }
+    /**
+     * 提现---从银行提现
+     * @return
+     */
+    @RequestMapping("/EN/main/pos_Bank")
+    public String ENpos_Bank() {
+        return "EN/main/pos_Bank"; }
+    /**
+     * 提现---从微信提现
+     * @return
+     */
+    @RequestMapping("/EN/main/pos_WeChat")
+    public String ENpos_WeChat() {
+        return "EN/main/pos_WeChat"; }
+
+    /**
+     * 提现---从支付宝提现
+     * @return
+     */
+    @RequestMapping("/EN/main/pos_Alipay")
+    public String ENpos_Alipay() {
+        return "EN/main/pos_Alipay"; }
+    /**
+     * 提现---现金提现
+     * @return
+     */
+    @RequestMapping("/EN/main/pos_cash")
+    public String ENpos_cash() {
+        return "EN/main/pos_cash"; }
+    /**
+     * 提现---比特币提现
+     * @return
+     */
+    @RequestMapping("/EN/main/pos_bitcoin")
+    public String ENpos_bitcoin() {
+        return "EN/main/pos_bitcoin"; }
+
+    /**
+     * 无卡取现
+     * @return
+     */
+    @RequestMapping("/EN/nonCardCash/cash_in")
+    public String ENnonCardCash() {
+        return "EN/nonCardCash/cash_in"; }
+
+    /**
+     * 智慧理财
+     * @return
+     */
+    @RequestMapping("/EN/more/financial")
+    public String ENfinancial() {
+        return "EN/more/financial"; }
+
+    /**
+     * 交易记录
+     * @return
+     */
+    @RequestMapping("/EN/more/trade_list")
+    public String ENtrade_list() {
+        return "EN/more/trade_list"; }
+    /**
+     * 历史交易记录
+     * @return
+     */
+    @RequestMapping("/EN/more/trade_history")
+    public String ENtrade_history() {
+        return "EN/more/trade_history"; }
+
+    /**
+     * 活动记录
+     * @return
+     */
+    @RequestMapping("/EN/more/active_list")
+    public String ENactive_list() {
+        return "EN/more/active_list"; }
 
 }
