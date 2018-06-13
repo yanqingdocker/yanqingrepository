fecce09ab5f2669f39df55012ac3210314693239
古月之胡清 <863643344@qq.com>
2018-06-13 11:35:38 +0800

    update

--- mmanager/src/main/java/cn/com/caogen/controller/MessageController.java
+++ mmanager/src/main/java/cn/com/caogen/controller/MessageController.java
@@ -2,13 +2,17 @@ package cn.com.caogen.controller;
 
 import cn.com.caogen.entity.Muser;
 import cn.com.caogen.entity.Task;
+import cn.com.caogen.entity.User;
 import cn.com.caogen.externIsystem.service.MessageService;
+import cn.com.caogen.service.IUserService;
 import cn.com.caogen.service.TaskServiceImpl;
 import cn.com.caogen.util.ConstantUtil;
 import cn.com.caogen.util.DateUtil;
 import cn.com.caogen.util.ResponseMessage;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -29,6 +33,11 @@ import java.util.stream.Stream;
 @RestController
 @RequestMapping("/message")
 public class MessageController {
+
+    private static Logger logger = LoggerFactory.getLogger(MessageController.class);
+
+    @Autowired
+    private IUserService userServiceImpl;
     @Autowired
     private TaskServiceImpl taskService;
     @RequestMapping("query")
@@ -44,6 +53,9 @@ public class MessageController {
         Stream<Task> stream=taskService.queryAll().stream();
         Task task=stream.filter((e)->e.getId()==id).collect(Collectors.toList()).get(0);
         String telphone=task.getTaskcontent();
+        User vipuser=getUser(telphone,null);
+        vipuser.setLeavel(1);
+        userServiceImpl.update(vipuser);
         String msg="尊敬的客户您好！恭喜您，你申请开通VIP经审核已经成功通过。";
         MessageService.sendMessage(telphone,msg);
         Muser user=(Muser)request.getSession().getAttribute("currentUser");
@@ -76,5 +88,18 @@ public class MessageController {
         taskService.updateTask(parmMap);
 
         return JSONObject.fromObject(new ResponseMessage(ConstantUtil.SUCCESS)).toString();
+    }
+
+    public User getUser(String telphone, String userid){
+        logger.info("getUser start: telphone="+telphone+",userid="+userid);
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("phone", telphone);
+        User temp = null;
+        List<User> userList=userServiceImpl.queryAll(map);
+        if(userList.isEmpty()){
+            return null;
+        }else {
+            return userList.get(0);
+        }
     }
 }
