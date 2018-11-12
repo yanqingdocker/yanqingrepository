cd087d33b47bb4cdb70626ec07d86d65380bb02e
zb <1227016053@qq.com>
2018-11-12 13:26:29 +0800

    Update qrcode/src/main/java/Qrcode.java

---
+++ qrcode/src/main/java/Qrcode.java
@@ -0,0 +1,15 @@
+import java.util.UUID;
+
+public class Qrcode {
+
+ public String index(){
+        try{
+            String uuid = UUID.randomUUID().toString();
+        }catch(Exception e){
+
+        }
+        
+        return "s";
+    }
+    
+}
